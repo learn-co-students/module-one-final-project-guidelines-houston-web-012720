b5b6626b0ac6f0560b7d06fb4aafea2c0688938c
Gavrilajava <georgy.gavrilchik@gmail.com>
2020-02-10 17:00:57 -0600

    my2cents

--- bin/run.rb
+++ bin/run.rb
@@ -1,25 +1,28 @@
 require_relative '../config/environment'
 require 'open-uri'
 
-
+def header
+    system ('clear')
+    puts $pastel.magenta.bold($a.asciify("Cora The Chooser"))
+end
 
 
 prompt = TTY::Prompt.new
-a = Artii::Base.new :font => 'slant'
+$a = Artii::Base.new :font => 'slant'
 remote_ip = open('http://whatismyip.akamai.com').read
 coordinates = Geokit::Geocoders::MultiGeocoder.geocode(remote_ip)
+$pastel = Pastel.new
 
 ###################### Initial screen, select category
 
-system ('clear')
-puts a.asciify("Cora The Chooser")
+header
 options = ["Eat and Drink","Going Out-Entertainment","Sights and Museums","Natural and Geographical","Transport","Accommodations","Leisure and Outdoor","Shopping","Business and Services","Facilities","Areas and Buildings"]
 category = prompt.select("Hello, what do you want to choose?", options)
  
 ###################### 2nd screen, choose Keywords
 
-system ('clear')
-puts a.asciify("Cora The Chooser")
+header
 puts "You've chosen #{category}, Great!"
+
 
 
