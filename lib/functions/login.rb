6747423ad3383c0fb905679c14f77d02658d4079
SteRobWms <stephen.r.williams.87@gmail.com>
2020-02-11 15:47:25 -0600

    built login

--- lib/functions/login.rb
+++ lib/functions/login.rb
@@ -1,9 +1,24 @@
 require_relative 'global'
+require 'pry'
+
+$current_student_id = 0
 
 def user_info
-    username = $prompt.ask('What is your name?', deafult: ENV['USER'])
-    password = $prompt.mask("What is your password?")
-    p "Hi #{username}! Your password is #{password}."
+    temp_username = $prompt.ask('What is your username?', deafult: ENV['USER'])
+    if !Student.find_by(username: temp_username)
+        puts "Sorry, username #{temp_username} is not yet created. Please create new user"
+    else
+        puts "Welcome back, #{temp_username}!"
+        temp_password = $prompt.mask("What is your password?")
+        if Student.find_by(username: temp_username).password == temp_password
+            $current_student_id = Student.find_by(username: temp_username).id
+            puts "Login (Great) Success!"
+        else
+            puts "Username and Password do not match. Please try again"
+        end
+    end
 end
 
-greeting
+# user_info
+# binding.pry
+0
