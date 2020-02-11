d1889d4fd4925e885bc3ec8d6601d55534fc23fb
vienpham2019 <vienpham2019@gmail.com>
2020-02-10 20:42:03 -0600

    added more methods

--- bin/run.rb
+++ bin/run.rb
@@ -6,19 +6,36 @@ def select_restaurant(location)
    Restaurant.where('location = ?',location).pluck(:restaurant_name)
 end
 
-def get_object(name)
+def get_object(name,user)
     a1 = Restaurant.where(restaurant_name: name).take 
     puts "The restaurant name is #{a1.restaurant_name}"
     puts "The location is #{a1.location}"
     puts "The restaurant type of food #{a1.type_of_food}"
+    puts "\n"
+    puts "Would you like to add this restaurant into your list (Yes/No)"
+    answer = gets.chomp
+    while true do
+        if answer == "Yes" 
+            add_to_list(a1,user)
+            puts "add to #{user.user_name} list"
+            puts "#{user.lists.restaurant.restaurant_name}"
+            break
+        elsif answer == "No"
+            break
+        else
+            puts "Invalid input please inter Yes or No to add restaurant to your list"
+            answer = gets.chomp
+        end 
+    end
+       
 end
 
-def list_of_restaurant(list)
+def list_of_restaurant(list,user)
     input = $prompt.select("abc".blue,list)
 
     for i in 0...input.length
         if input == list[i]
-            get_object(list[i])
+            get_object(list[i],user)
         end
     end
 
@@ -33,5 +50,5 @@ name = gets.chomp
 user = User.create(user_name: name)
 puts "Hi #{name}, puts your current location."
 location = gets.chomp
-list_of_restaurant(select_restaurant(location))
+list_of_restaurant(select_restaurant(location),user)
 
