1ef2271f9d832a2a4fdb10eab444040f6f69167c
github username <github email address>
2020-02-11 17:07:49 -0600

    Create journal entry

---
+++ lib/functions/create_journal.rb
@@ -0,0 +1,13 @@
+require_relative 'global'
+
+student_id =
+lecture_id = 
+date_created = 
+what_did_i_learn = 
+what_was_unclear = 
+confidence_level = 
+feelings_on_topic = 
+feelings_general = 
+mood = 
+
+LectureEntry.create(student_id = @current_student_id,)
