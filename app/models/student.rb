9dd61b45a71348d5e5a57a7195a3de75b07348d3
SteRobWms <stephen.r.williams.87@gmail.com>
2020-02-11 11:19:09 -0600

    seeds, class relations, and environment fixed

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,7 @@
 class Student < ActiveRecord::Base
     belongs_to :cohort
     has_many :journal_entries
-    has_many :lectures, through :journal_entries
+    has_many :lectures, through: :journal_entries
 
     def greeting
         p "Welcome #{username}!"
