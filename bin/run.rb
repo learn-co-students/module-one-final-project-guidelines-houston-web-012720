aebd35678176d60a2e85a8782d92eea6785c3a8d
GianMoran <gpmoran@uh.edu>
2020-02-11 15:25:50 -0600

    Wrong version

--- bin/run.rb
+++ bin/run.rb
@@ -1,32 +1,35 @@
 require_relative '../config/environment'
 # require_all
-$prompt = TTY::Prompt.new
+@prompt = TTY::Prompt.new
 
 def select_restaurant(location)
-   Restaurant.where('location = ?',location).pluck(:restaurant_name)
+    #Grabs the restaurant name from that location of the user and returns an array
+     Restaurant.where(location: location).pluck(:restaurant_name)
+    # binding.pry
 end
 
-def get_object(name)
-    a1 = Restaurant.where(restaurant_name: name).take 
-    puts "The restaurant name is #{a1.restaurant_name}"
-    puts "The location is #{a1.location}"
-    puts "The restaurant type of food #{a1.type_of_food}"
+def restaurants(name)
+    #grabs the first restaurant name of name being passed
+    restaurant = Restaurant.where(restaurant_name: name).take 
+
+    # grabs the name,location and type of food from restaurant
+    puts "The restaurant name is #{restaurant.restaurant_name}"
+    puts "The location is #{restaurant.location}"
+    puts "The restaurant type of food #{restaurant.type_of_food}"
 end
 
-def list_of_restaurant(list)
-    input = $prompt.select("abc".blue,list)
-
-    for i in 0...input.length
-        if input == list[i]
-            get_object(list[i])
-        end
-    end
+def list_of_restaurant(list_of_restaurant_name)
+    input = @prompt.select("abc".blue,list_of_restaurant_name)
+    restaurants(input)
 
 end
 
 def add_to_list(restaurant,user)
-    List.create(restaurant_id: restaurant.id, user_id: user.id)
+    
+    list = List.create(restaurant_id: restaurant.id, user_id: user.id)
+    l
 end
+
 
 puts "Please puts your name below"
 name = gets.chomp
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,15 +5,21 @@ Restaurant.destroy_all
 
 
 Restaurant.create(restaurant_name: "Olive Garden", location: "Houston", type_of_food: "Italian")
-Restaurant.create(restaurant_name: "Olive Garden1", location: "Houston", type_of_food: "Italian")
-Restaurant.create(restaurant_name: "Olive Garden2", location: "Houston", type_of_food: "Italian")
-Restaurant.create(restaurant_name: "Olive Garden3", location: "Conroe", type_of_food: "Italian")
-Restaurant.create(restaurant_name: "Olive Garden4", location: "Conroe", type_of_food: "Italian")
-Restaurant.create(restaurant_name: "Olive Garden5", location: "Conroe", type_of_food: "Italian")
-Restaurant.create(restaurant_name: "Olive Garden6", location: "Alabama", type_of_food: "Italian")
-Restaurant.create(restaurant_name: "Olive Garden7", location: "Alabama", type_of_food: "Italian")
-Restaurant.create(restaurant_name: "Olive Garden8", location: "Alabama", type_of_food: "Italian")
-Restaurant.create(restaurant_name: "Olive Garden9", location: "Houston", type_of_food: "Italian")
-Restaurant.create(restaurant_name: "Olive Garden10", location: "Houston", type_of_food: "Italian")
+Restaurant.create(restaurant_name: "Indigo", location: "Houston", type_of_food: "Italian")
+Restaurant.create(restaurant_name: "La Lucha", location: "Houston", type_of_food: "Mexican")
+Restaurant.create(restaurant_name: "Coltivare Piza & Garden", location: "Conroe", type_of_food: "Italian")
+Restaurant.create(restaurant_name: "BB's Tex-Orleans", location: "Conroe", type_of_food: "Southern")
+Restaurant.create(restaurant_name: "Nancy's Hustle", location: "Conroe", type_of_food: "breakfast")
+Restaurant.create(restaurant_name: "Uchi", location: "Austin", type_of_food: "Japanese")
+Restaurant.create(restaurant_name: "Fonda San Miguel", location: "Austin", type_of_food: "Mexican")
+Restaurant.create(restaurant_name: "Jeffery's", location: "Austin", type_of_food: "American")
+Restaurant.create(restaurant_name: "Valentina's Tex Mex", location: "Austin", type_of_food: "Mexican")
 
 
+response = RestClient.get('https://developers.zomato.com/api/v2.1/search?entity_id=277&entity_type=city&collection_id=1
+
+
+', headers={'user-key': "a9252d17baa2869cc9fb47f0db114837"})
+ parsed_data = JSON.parse(response)
+ binding.pry
+ 0
