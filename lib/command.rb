3a877dee5f15fc0b54c1fe5b60603cd48978575b
DrVonDevious <drvondevious@gmail.com>
2020-02-11 12:59:02 -0600

    add quit, go_south and go_north commands

--- lib/command.rb
+++ lib/command.rb
@@ -1,29 +1,32 @@
-module Command 
+module Command
+
     @@prompt = TTY::Prompt.new
-
-
 
     def get_command
         @@prompt.ask(">: ")
-
     end
 
     def do_command(cmd)
         case cmd 
-        when cmd == "go north"
+        when "go north"
             go_north
+        when "go south"
+            go_south   
+        when "where am i"
+            where_am_i    
+        when "quit"
+            Main.exit_game    
         end
-        
-
     end
 
     def go_north
-        
-
+      Main.class_variable_get(:@@current_trainer).area_id = 
+        Main.class_variable_get(:@@current_trainer).area.north_area_id
     end
 
     def go_south
-
+      Main.class_variable_get(:@@current_trainer).area_id = 
+        Main.class_variable_get(:@@current_trainer).area.south_area_id
     end
 
     def walk_in_grass
@@ -39,11 +42,8 @@ module Command
     end
 
     def where_am_i
-
+      place = Main.class_variable_get(:@@current_trainer).area.name
+      puts "You are in #{place}."
     end
-
-
-
-
 
 end
