5b1f0cb521a33f9849469b784f9df6b2f13c5a41
vienpham2019 <vienpham2019@gmail.com>
2020-02-11 15:19:35 -0600

    need debuging to delete function

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,22 @@ class User < ActiveRecord::Base
     has_many :lists
     has_many :restaurants, through: :lists
 
+    def add_to_list(restaurant)
+        List.create(restaurant_id: restaurant.id, user_id: self.id)
 
+        print_out_list
+    end
+
+    def print_out_list
+        rows = []
+        user = User.find(self.id)
+        user.restaurants.each{|restaurant| rows << [restaurant.restaurant_name,restaurant.location,restaurant.type_of_food]}
+        table = Terminal::Table.new :headings => ["Restaurant".light_blue,"Location".light_blue,"Food Type".light_blue], :rows => rows.uniq
+        puts table
+    end
+
+    def delete_from_lists(restaurant_name)
+        restaurant_id = restaurants.map{|restaurant| restaurant.id}
+        lists.destroy(restaurant_id)
+    end
 end
