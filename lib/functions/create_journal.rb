7e6dd1a6ed42f7f72b48cec16236dd9435e0cdf9
SteRobWms <stephen.r.williams.87@gmail.com>
2020-02-12 11:01:26 -0600

    added migration to auto-date journal entries

--- lib/functions/create_journal.rb
+++ lib/functions/create_journal.rb
@@ -13,7 +13,9 @@ mood = $prompt.select("What is your mood?", [1, 2, 3, 4, 5])
 
 # $current_student_id = 1
 
-JournalEntry.new(student_id: $current_student_id, what_did_i_learn: what_did_i_learn, what_was_unclear: what_was_unclear, confidence_level: confidence_level, feelings_on_topic: feelings_on_topic, feelings_general: feelings_general, mood: mood)
+j1 = JournalEntry.new(student_id: $current_student_id, what_did_i_learn: what_did_i_learn, what_was_unclear: what_was_unclear, confidence_level: confidence_level, feelings_on_topic: feelings_on_topic, feelings_general: feelings_general, mood: mood)
 
+binding.pry
+0
 # JournalEntry.create(student_id: $current_student_id, lecture_id: 1, date_created: 00000000, what_did_i_learn: "test", what_was_unclear: "nothing", confidence_level: 3, feelings_on_topic: "Happy", feelings_general: "Unhappy", mood: 5)
 
