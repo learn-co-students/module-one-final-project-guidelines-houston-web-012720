81e1cfd553601c14eb2b9e824991466fa1ca51c3
placidowang <placidow@gmail.com>
2020-02-12 16:36:27 -0600

    improved logic for Place#is_internal_link?, fixed run.rb skipping tag setting when gathering new API data

--- bin/run.rb
+++ bin/run.rb
@@ -5,14 +5,17 @@ require 'open-uri'
 ###################### Initial screen, select category
 Viewer.header
 options = ["Eat and Drink","Going Out-Entertainment","Sights and Museums","Natural and Geographical","Transport","Accommodations","Leisure and Outdoor","Shopping","Business and Services","Facilities","Areas and Buildings"]
+use_previous_data = false
 data_was_cleared = false
 if Place.count > 0 && Viewer.prompt.yes?("#{Place.count} place(s) already exist from the previous search. Would you like to use them?")
+    use_previous_data = true
     if Viewer.prompt.yes?("Would you like to reselect preferences?")
         Tag.wipe_relevance
         data_was_cleared = true
     end
 else
-    category = Viewer.prompt.select("Hello, which category are you interested in?", options)
+    data_was_cleared = true
+    category = Viewer.prompt.select("Hello, which category are you interested in?\n", options)
     puts "You've chosen #{category}, great!"
     Viewer.header
     APIScrapper.get_data(category)
@@ -21,7 +24,7 @@ end
 
 ###################### 2nd screen, select tags
 Viewer.header
-puts "Using data from the previous search" if data_was_cleared
+puts "Using data from the previous search" if use_previous_data
 tags = Tag.get_tag_names
 tags.unshift("DONE SELECTING TAGS")
 
@@ -31,7 +34,7 @@ while Tag.with_relevance.count < 5 do
     if tag == "DONE SELECTING TAGS"
         break
     end
-    rel =  Viewer.prompt.slider('How relevant is this tag?',  min: -100, max: 100, step: 5)
+    rel =  Viewer.prompt.slider('How relevant is this tag?\n',  min: -100, max: 100, step: 5)
     unless rel == 0
         tags -= [tag]
         Tag.find_by(title: tag).update(relevance: rel)
