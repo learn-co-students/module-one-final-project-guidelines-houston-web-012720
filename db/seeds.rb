9c817a0565f773d0fd5869234766b071fc6d00ec
GianMoran <gpmoran@uh.edu>
2020-02-12 18:01:12 -0600

    Fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,18 +13,18 @@
 # Restaurant.create(restaurant_name: "Jeffery's", location: "Austin", type_of_food: "American")
 # Restaurant.create(restaurant_name: "Valentina's Tex Mex", location: "Austin", type_of_food: "Mexican")
 
-houston_url = 'https://developers.zomato.com/api/v2.1/search?entity_id=277&entity_type=city&collection_id=1'
-austin_url = "https://developers.zomato.com/api/v2.1/search?entity_id=278&entity_type=city"
-dallas_url = "https://developers.zomato.com/api/v2.1/search?entity_id=276&entity_type=city"
-lubbock_url = "https://developers.zomato.com/api/v2.1/search?entity_id=1189&entity_type=city"
+# houston_url = 'https://developers.zomato.com/api/v2.1/search?entity_id=277&entity_type=city&collection_id=1'
+# austin_url = "https://developers.zomato.com/api/v2.1/search?entity_id=278&entity_type=city"
+# dallas_url = "https://developers.zomato.com/api/v2.1/search?entity_id=276&entity_type=city"
+# lubbock_url = "https://developers.zomato.com/api/v2.1/search?entity_id=1189&entity_type=city"
 
- response = RestClient.get(lubbock_url, headers={'user-key': "a9252d17baa2869cc9fb47f0db114837"})
- restaurant = JSON.parse(response)
- restaurant["restaurants"].each do |restaurant|
-    name = restaurant["restaurant"]["name"]
-    city = restaurant["restaurant"]["location"]["city"]
-    address = restaurant["restaurant"]["location"]["address"]
-    cuisines = restaurant["restaurant"]["cuisines"]
-    timing = restaurant["restaurant"]["timings"]
-    Restaurant.create(restaurant_name: name, city: city, address: address, type_of_food: cuisines, timing: timing)
- end
+#  response = RestClient.get(lubbock_url  , headers={'user-key': "a9252d17baa2869cc9fb47f0db114837"})
+#  restaurant = JSON.parse(response)
+#  restaurant["restaurants"].each do |restaurant|
+#     name = restaurant["restaurant"]["name"]
+#     city = restaurant["restaurant"]["location"]["city"]
+#     address = restaurant["restaurant"]["location"]["address"]
+#     cuisines = restaurant["restaurant"]["cuisines"]
+#     timing = restaurant["restaurant"]["timings"]
+#     Restaurant.create(restaurant_name: name, city: city, address: address, type_of_food: cuisines, timing: timing)
+#  end
